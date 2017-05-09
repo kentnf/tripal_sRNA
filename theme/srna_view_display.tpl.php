88605033d085a129c5fe646c8d9caa143e13aa64
kentnf <kentnf@gmail.com>
2017-05-09 15:37:46 -0400

    update

--- theme/srna_view_display.tpl.php
+++ theme/srna_view_display.tpl.php
@@ -6,13 +6,11 @@ if ($_SESSION['tripal_sirnaview_search']['METHOD']) {
   $jb_data     = $_SESSION['tripal_sirnaview_search']['data'];
   $jb_location = $_SESSION['tripal_sirnaview_search']['location'];
   $jb_tracks   = $_SESSION['tripal_sirnaview_search']['tracks'];
-  $tracks = implode(",", $_SESSION['tripal_sirnaview_search']['tracks']);
+  $tracks = 'DNA,genes,' . implode(",", $_SESSION['tripal_sirnaview_search']['tracks']);
 
-  // data for test
-  $jb_data = 'melon_v351';
-  $jb_location = 'MELO3C026828';
-  $jb_tracks = 'DNA,gene';
-
+  //dpm($jb_data);
+  //dpm($jb_location);
+  //dpm($tracks);
   unset($_SESSION['tripal_sirnaview_search']['METHOD']);
 
   $jbrowse_frame = "<iframe style=\"border: 1px solid rgb(80, 80, 80);\" src=\"/JBrowse/?data=icugi_data/json/$jb_data&loc=$jb_location&tracks=$tracks&tracklist=0&nav=1&overview=0\" height=\"650\" width=\"850\" name=\"jbrowse_iframe\">
